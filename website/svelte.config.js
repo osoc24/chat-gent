7a93a5243f5b7c9bee06c57910ac6516d2a35cdc
Robin van der Linde <robin.vanderlinde@gmail.com>
2024-07-18 18:25:21 +0200

    Fix base path

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -12,13 +12,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: false
+			paths: {
+				base: process.argv.includes('dev') ? '' : '/chat-gent/'
+			}
 		})
 	}
 };
